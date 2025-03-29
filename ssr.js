4648bbde1fad5d4bf0f031e019f61ff650a1048a
Somanshu Singh <somanshS14@gmail.com>
2025-03-30 01:26:54 +0530

    changes to ssr js

--- ssr.js
+++ ssr.js
@@ -1,11 +1,31 @@
-// Immediately invoked function to run before the rest of the DOM is fully parsed.
 (function() {
-    // Create a custom meta tag
-    var metaCustom = document.createElement('meta');
-    metaCustom.setAttribute('name', 'custom-meta');
-    metaCustom.setAttribute('content', 'This is a custom meta tag for SSR simulation.');
-    
-    // Append the custom meta tag to the document head
-    document.head.appendChild(metaCustom);
+    // Helper function to create and append meta tags
+    function addMetaTag(attrName, attrValue, content) {
+      var meta = document.createElement('meta');
+      meta.setAttribute(attrName, attrValue);
+      if(content) {
+        meta.setAttribute('content', content);
+      }
+      document.head.appendChild(meta);
+    }
+  
+    // HTML Meta Tags
+    document.title = "Fastform - The best alternative to Google Forms and Typeform";
+    addMetaTag("name", "description", "Fastform is the best alternative to Google Forms and Typeform. Auto-optimized for speed and delight.");
+  
+    // Facebook Meta Tags
+    addMetaTag("property", "og:url", "https://fastform.in");
+    addMetaTag("property", "og:type", "website");
+    addMetaTag("property", "og:title", "Fastform - The best alternative to Google Forms and Typeform");
+    addMetaTag("property", "og:description", "Fastform is the best alternative to Google Forms and Typeform. Auto-optimized for speed and delight.");
+    addMetaTag("property", "og:image", "https://fastform.in/og-image.png");
+  
+    // Twitter Meta Tags
+    addMetaTag("name", "twitter:card", "summary_large_image");
+    addMetaTag("property", "twitter:domain", "fastform.in");
+    addMetaTag("property", "twitter:url", "https://fastform.in");
+    addMetaTag("name", "twitter:title", "Fastform - The best alternative to Google Forms and Typeform");
+    addMetaTag("name", "twitter:description", "Fastform is the best alternative to Google Forms and Typeform. Auto-optimized for speed and delight.");
+    addMetaTag("name", "twitter:image", "https://fastform.in/og-image.png");
   })();
   
